9d2c6e7753923744b94e77dffe4a4d66d46320a2
shawtypimp <112937058+shawtypimp@users.noreply.github.com>
2024-11-12 14:54:21 +0300

    add method that checks data of creation habit completion

---
+++ trackYourHabits/src/main/java/com/guccikray/trackYourHabits/exceptions/HabitAlreadyMarkedAsCompletedTodayException.java
@@ -0,0 +1,7 @@
+package com.guccikray.trackYourHabits.exceptions;
+
+public class HabitAlreadyMarkAsCompletedTodayException extends Exception{
+    public HabitAlreadyMarkAsCompletedTodayException(String message) {
+        super(message);
+    }
+}
